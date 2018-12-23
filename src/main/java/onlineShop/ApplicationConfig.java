a35669fd2df552969249cbc42c366bfa3fa5deac
wangyishi <wangyishistl@gmail.com>
2018-12-23 15:51:07 -0600

    complete version 1

---
+++ src/main/java/onlineShop/ApplicationConfig.java
@@ -0,0 +1,75 @@
+package onlineShop;
+
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.security.web.firewall.HttpFirewall;
+import org.springframework.security.web.firewall.StrictHttpFirewall;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+
+@Configuration
+public class ApplicationConfig {
+
+	@Bean(name = "sessionFactory")
+	public LocalSessionFactoryBean sessionFactory() {
+		LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+		sessionFactory.setDataSource(dataSource());
+		sessionFactory.setPackagesToScan("onlineShop.model");
+		sessionFactory.setHibernateProperties(hibernateProperties());
+		return sessionFactory;
+	}
+
+	@Bean(name = "dataSource")
+	public DataSource dataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+		
+		// TODO: Change port according to your database settings
+dataSource.setUrl("jdbc:mysql://localhost:3306/ecommerce?serverTimezone=UTC");
+		dataSource.setUsername("root");
+		dataSource.setPassword("root");
+
+		return dataSource;
+	}
+	
+	
+    // Handler for multipart form data
+    @Bean
+    public MultipartResolver multipartResolver() {
+   	 CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
+   	 multipartResolver.setMaxUploadSize(10240000);
+   	 return multipartResolver;
+    }
+    
+    // Properly encode URL encoding for special characters.
+    @Bean
+    public HttpFirewall allowUrlEncodedSlashHttpFirewall() {
+    	StrictHttpFirewall firewall = new StrictHttpFirewall();
+    	firewall.setAllowUrlEncodedSlash(true);
+    	firewall.setAllowSemicolon(true);
+    	return firewall;
+    }
+
+
+	
+
+	private final Properties hibernateProperties() {
+		Properties hibernateProperties = new Properties();
+		hibernateProperties.setProperty("hibernate.hbm2ddl.auto", "update");// use "create-drop" to reset database; use "update" to keep databbase 
+		hibernateProperties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
+		hibernateProperties.setProperty("hibernate.show_sql", "true");
+
+		return hibernateProperties;
+	}
+}
+
+
